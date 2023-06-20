942c286d65c2a32a04b95dbb170777f00a88bdf7
Roman <roman.qwonix@gmail.com>
2023-06-21 00:24:26 +0300

    init

---
+++ src/main/java/ru/qwonix/telegram/movieplayerbot/MoviePlayerBotApplication.java
@@ -0,0 +1,13 @@
+package ru.qwonix.telegram.movieplayerbot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoviePlayerBotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MoviePlayerBotApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/qwonix/telegram/movieplayerbot/MoviePlayerBotApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.qwonix.telegram.movieplayerbot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoviePlayerBotApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
